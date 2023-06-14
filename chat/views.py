12631a76ad091712712342ad2905ce6df932bfc2
pnaskardev <priyanshunaskar89@gmail.com>
2023-06-14 18:47:40 +0530

    added some part of frontend

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .models import Room
+
+@require_POST
+def create_room(request,uuid):
+    name=request.POST.get('name','')
+    url=request.POST.get('url','')
+
+    room=Room.objects.create(uuid=uuid,client=name,url=url)
+
+    return JsonResponse({'message':'Room Created'})
