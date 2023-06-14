0a31927396117ca1b2948addaf9248a6c5996fe6
pnaskardev <priyanshunaskar89@gmail.com>
2023-06-14 14:29:51 +0530

    basic setup complete and now onto configuring the channels

--- talk/settings.py
+++ talk/settings.py
@@ -35,7 +35,8 @@ LOGOUT_REDIRECT_URL = '/' # new
 
 CHANNEL_LAYERS = { 
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        
     }
 }
 
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
