03d17cbb1ccd291d7a50d2509b1b4b2fd9f7a43f
pnaskardev <priyanshunaskar89@gmail.com>
2023-06-15 10:52:29 +0530

    completed the project

--- chat/consumers.py
+++ chat/consumers.py
@@ -29,12 +29,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Inform the user here
+        if self.user.is_staff:
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'users_update',
+                }
+            )
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+        if not self.user.is_staff:
+            await self.set_room_closed()
+
+    async def users_update(self, event):
+        # send information to the web socket 
+        await self.send(text_data=json.dumps({
+            'type': 'users_update'
+        }))
+    
 
     async def receive(self, text_data):
 
@@ -49,9 +67,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         print('Recieve:', type)
 
         if type == 'message':
-
             new_message = await self.create_message(name, message, agent)
-
             # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name, {
@@ -61,6 +77,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'agent': agent,
                     'initials': initials(name),
                     'created_at': timesince(new_message.created_at)
+                }
+            )
+        elif type=='update':
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'writing_active',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
                 }
             )
 
@@ -73,6 +99,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'agent': event['agent'],
             'initials': event['initials'],
             'created_at': event['created_at']
+        }))
+
+
+    async def writing_active(self,event):
+        await self.send(text_data=json.dumps({
+            'type': event['type'],
+            'message': event['message'],
+            'name': event['name'],
+            'agent': event['agent'],
+            'initials': event['initials'],
         }))
 
     @sync_to_async
@@ -90,3 +126,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room.messages.add(message)
 
         return message
+    
+    @sync_to_async
+    def set_room_closed(self):
+        self.room=Room.objects.get(uuid=self.room_name)
+        self.room.status=Room.CLOSED
+        self.room.save()
